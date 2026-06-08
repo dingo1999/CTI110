9e2b2bcbd81cc7651589487657533645e925174e
dingo1999 <hutchink1024@student.faytechcc.edu>
2026-06-08 15:12:40 -0400

    Add files via upload

---
+++ P1LAB1_HutchinsonKen.py
@@ -0,0 +1,8 @@
+# Kenneth Hutchinson
+# 8 June 26
+# P1LAB1_HutchinsonKen
+# Prompt for First and Last Name
+
+first_name = input ("Enter Your First Name: ")
+last_Name = input ("Enter your last name: ")
+print ("Hello," , first_name, last_Name +"!", "Welcone to CTI-100!")
